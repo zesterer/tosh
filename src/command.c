4b68f8881795ec8972f3d4d9b3b90f87058ccd80
zesterer <barry.of.smith@gmail.com>
2016-04-15 09:36:10 +0100

    Added Ccd recognition and improved error output

--- src/command.c
+++ src/command.c
@@ -3,32 +3,57 @@
 #include "stdio.h"
 #include "string.h"
 #include "unistd.h"
+#include "errno.h"
 
 // Local
 #include "command.h"
+#include "config.h"
 
 int toshCommand(const char* command, char* out, int max_len)
 {
-	FILE* stream = popen(command, "r");
-
-	unsigned int pos = 0;
-	while (!feof(stream))
+	if (strncmp(command, "cd", 2) == 0)
 	{
-		char cchar;
+		char location[MAX_LINE];
+		strcpy(location, command + 3);
+		int loc_len = strlen(location);
+		if (location[loc_len - 1] == '\n')
+			location[loc_len - 1] = '\0';
 
-		if(pos < max_len)
+		if (loc_len != 0)
 		{
-			cchar = fgetc(stream);
-			out[pos] = cchar;
-			pos ++;
+			if (chdir(location) != 0)
+				printf("%s\n", strerror(errno));
 		}
 		else
-			break;
+		{
+			if (chdir(getenv("HOME")) != 0)
+				printf("%s\n", strerror(errno));
+		}
+		out[0] = '\0';
 	}
-	out[pos - 1] = '\0';
-	fclose(stream);
+	else
+	{
+		FILE* stream = popen(command, "r");
 
-	//printf("Command : %s\n", out);
+		unsigned int pos = 0;
+		while (!feof(stream))
+		{
+			char cchar;
+
+			if(pos < max_len)
+			{
+				cchar = fgetc(stream);
+				out[pos] = cchar;
+				pos ++;
+			}
+			else
+				break;
+		}
+		out[pos - 1] = '\0';
+		fclose(stream);
+
+		//printf("Command : %s\n", out);
+	}
 
 	return 0;
 }
